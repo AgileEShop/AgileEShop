782508dc9b052ebd3a73e05d69b0a23e30d127dc
776275371@qq.com <1997Victorique0317>
2020-02-03 16:38:58 +0800

    delete a module

--- eshop-user/src/main/java/cn/nju/edu/eshop/user/mapper/UserReceiveAddressMapper.java
+++
@@ -1,7 +0,0 @@
-package cn.nju.edu.eshop.user.mapper;
-
-import cn.nju.edu.eshop.bean.UserReceiveAddress;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface UserReceiveAddressMapper extends Mapper<UserReceiveAddress> {
-}
