fc70027b85b5a15d9b9e4b736293b60982001662
776275371@qq.com <1997Victorique0317>
2020-02-06 15:24:22 +0800

    Update. Manage service and manage web can be seen on dubbo-admin

--- eshop-manage-service/src/main/java/cn/nju/edu/eshop/manage/EshopManageServiceApplication.java
+++ eshop-manage-service/src/main/java/cn/nju/edu/eshop/manage/EshopManageServiceApplication.java
@@ -2,8 +2,10 @@ package cn.nju.edu.eshop.manage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+@MapperScan(basePackages = "cn.nju.edu.eshop.manage.mapper")
 public class EshopManageServiceApplication {
 
     public static void main(String[] args) {
